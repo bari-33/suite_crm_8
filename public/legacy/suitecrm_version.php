e9bd63e9f5849001321d933d8ced1f9e8eb7aab0
= <test@w3schools.com>
2022-12-13 12:09:11 +0500

    first commit

---
+++ public/legacy/suitecrm_version.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$suitecrm_version = '8.2.1';
+$suitecrm_legacy = '7.12.8';
+$suitecrm_timestamp = '2022-11-17 12:00:00';
